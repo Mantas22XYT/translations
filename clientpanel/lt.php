6cdcda354466c4f8d609fb54c4f3e9b32602b05b
Mantas22XYT <75636222+Mantas22XYT@users.noreply.github.com>
2023-02-24 23:28:58 +0200

    Create lt.php
    STOPPED AT LINE 200

---
+++ clientpanel/lt.php
@@ -0,0 +1,269 @@
+<?php
+
+// page titles
+$lang['title']['mainpage_title'] = "Peržiūrėti serverius";
+$lang['title']['mainpage_description'] = "Peržiūrėkite ir valdykite savo Minecraft serverius.";
+
+$lang['title']['createserver_title'] = "Kurti serverį";
+$lang['title']['createserver_description'] = "Sukurk savo Minecraft serverį.";
+
+$lang['title']['changeram_title'] = "Pakeisti RAM";
+$lang['title']['changeram_description'] = "Modifikuokite savo serverio resursus.";
+
+$lang['title']['alwaysonline_title'] = "Visada Įjungtas";
+$lang['title']['alwaysonline_description'] = "Kuriama.";
+
+$lang['title']['coins_title'] = "\"Coins\"";
+$lang['title']['coins_description'] = "Gaukite \"coins\" kad paspartintumėte savo serverius.";
+
+// navigation bar
+$lang['nav']['dashboard'] = "Valdymo skydas";
+$lang['nav']['coins'] = "\"Coins\"";
+$lang['nav']['mainpage'] = "Pagrindinis puslapis";
+$lang['nav']['servers'] = "Serveriai";
+$lang['nav']['createserver'] = "Kurti serverį";
+$lang['nav']['changeram'] = "Pakeisti RAM";
+$lang['nav']['alwaysonline'] = "Visada Įjungtas";
+$lang['nav']['soon'] = "Tuoj";
+$lang['nav']['account'] = "Paskyra";
+$lang['nav']['logintopanel'] = "Prisijungti prie penelės";
+$lang['nav']['plandetails'] = "Plano detalės";
+$lang['nav']['billing'] = "Mokėjimai";
+
+// errors
+$lang['error']['login_is_required'] = "Reikalingas prisijungimas!";
+$lang['error']['login_is_required_description'] = "Prisijunkite kad matytumėte savo serverius.";
+$lang['error']['login_is_required_button'] = "Prisijungti";
+
+$lang['error']['create_new_server'] = "Sukurkite savo paties serverį!";
+$lang['error']['create_new_server_description'] = "Savo pirmąjį serverį galite sukurti per kelis paspaudimus.";
+$lang['error']['create_new_server_button'] = "Kurti serverį";
+
+$lang['error']['construction'] = "Šis puslapis dar kuriamas!";
+$lang['error']['construction_description'] = "Šiuo metu mes kuriame šį puslapį, tad prisėsk ir suvalgyk cepelinų.";
+
+// notices
+$lang['notices']['premiumdiscount'] = "Gauk <b>10% nuolaid 6 mėnesiams</b> su kuponu <b>UPGRADE10</b> naujinant savo planą.";
+$lang['notices']['premiumad'] = "Hostink savo serverį ant geriausios geležies su papildomomis funkcijomis, kaip \"24/7 online\" ir be reklamų naudojant mūsų Premium planus su kainomis kurios yra žemiau rinkos! <a href='https://falixnodes.net/minecraft-server-hosting'>Spausk čia</a>!";
+
+// widgets
+$lang['widgets']['panel_title'] = "Tavo panelės detalės";
+$lang['widgets']['panel_ftp'] = "Tavo FTP slaptažodis";
+$lang['widgets']['panel_resetpass'] = "Netinkamas slaptažodis? Pakeisk jį <a href='/profile/settings'>profilio nustatymuose</a>.";
+$lang['widgets']['panel_loginbutton'] = "Prisijunk prie panelės";
+$lang['widgets']['panel_closebutton'] = "Uždaryti";
+
+$lang['widgets']['changecpu_title'] = "Pakeisti CPU";
+$lang['widgets']['changecpu_newcpu'] = "Nauji serverio CPU branduoliai";
+$lang['widgets']['changecpu_changebutton'] = "Pakeisti CPU";
+$lang['widgets']['changecpu_closebutton'] = "Uždaryti";
+
+$lang['widgets']['changedisk_title'] = "Pakeisti disko talpą";
+$lang['widgets']['changedisk_newcpu'] = "Nauja serverio disko talpa (Megabaitais)";
+$lang['widgets']['changedisk_changebutton'] = "Pakeisti disko talpą";
+$lang['widgets']['changedisk_closebutton'] = "Uždaryti";
+
+// quick links
+$lang['links']['quicklinks'] = "Greitos nuorodos";
+$lang['links']['quicklinks_description'] = "Keliaukite po mūsų valdymo skydą su greitomis nuorodomis!";
+$lang['links']['createserver'] = "Sukurkite savo serverį";
+$lang['links']['createserver_description'] = "Sukurkite savo Minecraft serverį";
+$lang['links']['coinshop'] = "Apsipirkite FalixCoins parduotuvėje";
+$lang['links']['coinshop_description'] = "Pirkite paspartinimus savo serveriams";
+$lang['links']['coins'] = "Gaukite FalixCoins nemokamai";
+$lang['links']['coins_description'] = "Paspartinkite savo serverį gaunant mūsų virtualią valiutą";
+$lang['links']['discordsupport'] = "Discord palaikymas";
+$lang['links']['discordsupport_description'] = "Paklauskite mūsų bendruomenės apie jūsų turimus klausimus";
+
+// resources widget
+$lang['resources']['serversbalance'] = "Serverių balansas";
+$lang['resources']['rambalance'] = "RAM balansas";
+$lang['resources']['cpubalance'] = "CPU balansas";
+$lang['resources']['diskbalance'] = "Disko talpos balansas";
+$lang['resources']['databasesbalance'] = "Duomenų bazių balansas";
+$lang['resources']['portsbalance'] = "Prievadų balansas";
+
+// login page
+$lang['auth']['login_title'] = "FalixNodes";
+$lang['auth']['login_description'] = "Gaukite savo nemokamą hostingą!";
+$lang['auth']['have_an_account'] = "Turite paskyrą?";
+$lang['auth']['have_an_account_button'] = "Prisijunkite";
+$lang['auth']['dont_have_account_yet'] = "Neturite paskyros? <br><a class='text-decoration-underline link-light' href='https://client.falixnodes.net/auth/register'>Pradėkite!</a>";
+$lang['auth']['useragreement'] = "Perskaitykite <a class='text-decoration-underline text-white' href='https://falixnodes.net/user-agreement'>vartotojo susitarimą.</a>";
+$lang['auth']['loginform_title'] = "Paskyros Prisijungimas";
+$lang['auth']['form_email'] = "E. pašto adresas";
+$lang['auth']['form_password'] = "Slaptažodis";
+$lang['auth']['form_confirmpassword'] = "Patvirtinkite slaptažodį";
+$lang['auth']['loginform_forgotpassword'] = "Pamiršote slaptažodį?";
+$lang['auth']['loginform_button'] = "Prisijunkite";
+$lang['auth']['oauthlogins'] = "arba naudojant";
+$lang['auth']['register_oauthlogins'] = "arba prisiregistruokite naudojant";
+$lang['auth']['google'] = "Google";
+$lang['auth']['discord'] = "Discord";
+$lang['auth']['registerform_title'] = "Registruotis";
+$lang['auth']['registerform_button'] = "Registruotis";
+$lang['auth']['registerform_tosaccept'] = "Aš sutinku su <a href='https://falixnodes.net/user-agreement'>naudotojo susitarimu </a>ir <a href='https://falixnodes.net/privacy-policy'>privatumo politika</a>";
+$lang['auth']['registerform_marketingconsent'] = "Pažymėkite jei norite gauti žinutes į jūsų e. paštą su naujausiais pasiūlymais.";
+
+// index.php / main page
+$lang['manage_servers'] = "Valdykite savo serverius naudojant mūsų žaidimų panelę.";
+
+// servers table
+$lang['servers'] = "Tavo serveriai";
+$lang['logintogamepanel'] = "Prisijunkite prie žaidimų panelės";
+$lang['logintogamepanel_button'] = "Žaidimų panelė";
+
+$lang['server']['name'] = "SERVERIO VARDAS";
+$lang['server']['cpu'] = "CPU";
+$lang['server']['ram'] = "RAM";
+$lang['server']['disk'] = "DISKO TALPA";
+$lang['server']['database'] = "DUOMENŲ BAZĖS";
+$lang['server']['port'] = "PRIEVADAI";
+
+$lang['server']['gotoserver'] = "Eiti į Serverį";
+$lang['server']['changecpu'] = "Keisti CPU";
+$lang['server']['changeram'] = "Keisti RAM";
+$lang['server']['changedisk'] = "Keisti disko talpą";
+$lang['server']['deleteserver'] = "Pašalinti serverį";
+
+// create server page
+$lang['create']['createserver'] = "Kurti serverį";
+$lang['create']['servername'] = "Serverio vardas";
+$lang['create']['serverram'] = "Serverio RAM";
+$lang['create']['servergame'] = "Žaidimas";
+$lang['create']['allgames'] = "Visi palaikomi žaidimai";
+$lang['create']['selectgame'] = "Pasirinkite žaidimą";
+$lang['create']['minecrafteula'] = "Sutinku su <a href='https://account.mojang.com/documents/minecraft_eula/'>Minecraft End User License Agreement (EULA)</a>";
+$lang['create']['createbutton'] = "Kurti";
+$lang['create']['adblock'] = "((( Jei mygtukas neveikia nuo 3 iki 5 sekundžių, išjunkite savo AdBlock )))";
+
+// change ram page
+$lang['changeram']['title'] = "Keisti RAM";
+$lang['changeram']['servername'] = "Serverio pavadinimas";
+$lang['changeram']['currentram'] = "Dabartinis RAM kiekis";
+$lang['changeram']['newram'] = "Naujas RAM kiekis";
+$lang['changeram']['changerambutton'] = "Keisti RAM";
+
+// coins page
+$lang['coins']['earnfalixcoins'] = "Gaukite FalixCoins";
+$lang['coins']['resourcestore'] = "FalixCoins parduotuvė";
+$lang['coins']['task'] = "Užuotis";
+$lang['coins']['reward'] = "Atlygis";
+$lang['coins']['earn'] = "Gaukite";
+$lang['coins']['claim'] = "Atsiimkite";
+$lang['coins']['invitefriends'] = "Pakvieskite savo draugus";
+$lang['coins']['joinforrewards'] = "Prisijunkite už atlygį";
+$lang['coins']['invitereward'] = "1,000 FalixCoins už kiekvieną pakvoestą draugą";
+$lang['coins']['joinreward'] = "iki 50 FalixCoins už serverį";
+
+// affiliate page
+$lang['affiliate']['title'] = "Pakvieskite draugą ir gaukite 1,000 FalixCoins";
+$lang['affiliate']['description'] = "Pakvieskite draugus ir gaukite savo atlygį per kelias sekundes.";
+$lang['affiliate']['seconddescription'] = "Pakvieskite draugus nukopijuojant svetainės nuorodą iš žemiau it nusiunčiant savo draugams.";
+$lang['affiliate']['footer'] = "Kai pakvietėte savo draugus, galite <a href='https://client.falixnodes.net/affiliate/status'>peržiūrėti nuorodos statusą</a> arba parašykite mums jei turite klausimų.";
+
+// activity page
+$lang['activity']['title'] = "Aktyvumas";
+$lang['activity']['id'] = "ID";
+$lang['activity']['action'] = "VEIKSMAS";
+$lang['activity']['description'] = "APRAŠYMAS";
+$lang['activity']['useragent'] = "USER-AGENT";
+$lang['activity']['ip'] = "IP ADRESAS";
+$lang['activity']['date'] = "DATA";
+
+// profile page
+$lang['profile']['settings_title'] = "Profilio nustatymai";
+$lang['profile']['firstname'] = "Vardas";
+$lang['profile']['lastname'] = "Pavardė";
+$lang['profile']['email'] = "E. paštas";
+$lang['profile']['theme'] = "Svetainės tema";
+$lang['profile']['textcolor'] = "Teksto spalva";
+
+$lang['profile']['serversettings_title'] = "Serverio nustatymai";
+$lang['profile']['externalserverstart'] = "Išorinė serverio paleistis";
+$lang['profile']['update'] = "Atnaujinti";
+$lang['profile']['enable'] = "Įjungti";
+$lang['profile']['disable'] = "Išjungti";
+
+$lang['profile']['emailpreference_title'] = "E. pašto pasirinkimas";
+$lang['profile']['emailpreference'] = "Keisti e. pašto pasirinkimą";
+
+$lang['profile']['changepassword_title'] = "Keisti slaptažodį";
+$lang['profile']['changepassword_notice'] = "DĖMESIO! Jei bandote pakeisti žaidimų panelės slaptažodį, šiek tiek pslinkite žemyn.";
+$lang['profile']['oldpassword'] = "Senas slaptažodis";
+$lang['profile']['newpassword'] = "Naujas slaptažodis";
+$lang['profile']['confirmpassword'] = "Patvirtinkite slaptažodį";
+$lang['profile']['updatepassword'] = "Atnaujinti slaptažodį";
+
+$lang['profile']['discord'] = "Discord";
+$lang['profile']['authorised'] = "Prisijungta kaip";
+$lang['profile']['linkdiscord'] = "Prijungti Discord";
+$lang['profile']['unlinkdiscord'] = "Unlink Discord";
+$lang['profle']['notlinked'] = "You do not have Discord account linked to the account, to link your Discord account, authorise your Discord account";
+
+$lang['profile']['dangerzone'] = "Danger Zone";
+$lang['profile']['changegamepanelpassword_title'] = "Change Game Panel's password";
+$lang['profile']['changegamepanelpassword_description'] = "Once you change the Game Panel password, old password will no longer work.";
+$lang['profile']['changegamepanelpassword_button'] = "Change Password";
+$lang['profile']['deleteaccount_title'] = "Delete this account";
+$lang['profile']['deleteaccount_description'] = "Once you delete a account, there is no going back. Please be certain.";
+$lang['profile']['deleteaccount_button'] = "Delete Account";
+
+// falixcoins store
+$lang['store']['plan_expire_on'] = "Your plan will auto renew at";
+$lang['store']['month'] = "mo";
+$lang['store']['cancelplan'] = "Cancel Plan";
+$lang['store']['purchase'] = "Purchase Now";
+$lang['store']['booster'] = "Booster";
+
+$lang['store']['booster1_servers'] = "Create up to 3 servers";
+$lang['store']['booster1_cpu'] = "Get 120% CPU Power/each server";
+$lang['store']['booster1_ram'] = "Get 6 GB memory";
+$lang['store']['booster1_disk'] = "Get 15 GB disk/each server";
+$lang['store']['booster1_mysql'] = "Get 3 MySQL databases/each server";
+$lang['store']['booster1_ports'] = "Get 2 Extra Ports/each server";
+
+$lang['store']['booster2_servers'] = "Create up to 4 servers";
+$lang['store']['booster2_cpu'] = "Get 130% CPU Power/each server";
+$lang['store']['booster2_ram'] = "Get 7 GB memory";
+$lang['store']['booster2_disk'] = "Get 15 GB disk/each server";
+$lang['store']['booster2_mysql'] = "Get 3 MySQL databases/each server";
+$lang['store']['booster2_ports'] = "Get 2 Extra Ports/each server";
+
+$lang['store']['booster3_servers'] = "Create up to 5 servers";
+$lang['store']['booster3_cpu'] = "Get 140% CPU Power/each server";
+$lang['store']['booster3_ram'] = "Get 8 GB memory";
+$lang['store']['booster3_disk'] = "Get 15 GB disk/each server";
+$lang['store']['booster3_mysql'] = "Get 3 MySQL databases/each server";
+$lang['store']['booster3_ports'] = "Get 2 Extra Ports/each server";
+
+// tutorials
+$lang['tutorials']['createserver_stepone_title'] = "Create your server";
+$lang['tutorials']['createserver_stepone'] = "Your first time being here? No worries!<br />
+Start using Falix by creating your first server. Navigate to <a href='/create' target='_blank'>Create Server page</a> and start the creation process by filling out the form.<br />
+<br />
+<b>Server Name:</b> Name your server like 'Friends Server' or 'My Server'.<br />
+<b>Server RAM:</b> This might be bit confusing, but don't worry! Read our explanation by <a data-bs-toggle='offcanvas' data-bs-target='#RAMExplanation' aria-controls='RAMExplanation' style='text-decoration: underline dotted; cursor: pointer;' href='#'>clicking here</a>.<br />
+<br />
+After you have filled the form, click 'Create' and... congratulations! Your first own server has been created. Navigate to 'Manage your server' to find out how to access it.";
+
+$lang['tutorials']['createserver_steptwo_title'] = "Manage your server";
+$lang['tutorials']['createserver_steptwo'] = "You have created your first server, what next?<br />
+Simple enough, you login to your game panel account by clicking the button 'Login to Game Panel' in the right corner of the servers table.<br />
+<br />
+After it, you will see game panel interface. Find your server in the interface and click on it. After it, make sure to navigate to Versions tab which can be found in the navigation bar.<br />
+<br />
+You will be then asked to select most appropriate software and version for your server. We recommend you to start with Paper since it is most optimized software available for all versions. Then please find your version and select it.<br />
+<br />
+Once you have done that, your server has been installed. Now navigate to your server console and start the server by clicking the button 'Start'.<br />
+<br />
+Awesome, your server is now running! Head to next tutorial to figure out how to connect to it.";
+
+$lang['tutorials']['createserver_stepthree_title'] = "Connect to your server";
+$lang['tutorials']['createserver_stepthree'] = "Your server is running, what next?<br />
+Time to play on it! But how?<br />
+<br />
+Start your Minecraft client with the version you have selected in the game panel. Done? Alrightly.<br />
+You should now find your server unique hostname and its port, it can be found in bottom of the Console page. It usually starts with <b>game#.falixserver.net:00000</b>. Found it? Select it and paste it in your Minecraft multiplayer.<br />
+<br />
+Connect to it and... Awesome, you are in! Have fun with your friends! Simple as that.";
